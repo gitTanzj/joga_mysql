155646986d874d7b2e560292b2ae4a2b0745e905
gitTanzj <kalle.riit@gmail.com>
2023-11-01 14:27:25 +0200

    Displayed author article list

--- index.js
+++ index.js
@@ -47,12 +47,23 @@ app.get('/', function(req, res) {
 
 
 app.get('/article/:slug', (req, res) => {
-    let query = `SELECT author.name AS author, article.image AS image, article.published AS published, article.name AS name, article.body AS body FROM article JOIN author ON article.author_id = author.id WHERE slug = "${req.params.slug}";`
+    let query = `SELECT author.name AS author, author_id AS author_id, article.image AS image, article.published AS published, article.name AS name, article.body AS body FROM article JOIN author ON article.author_id = author.id WHERE slug = "${req.params.slug}";`
     let article = [];
     con.query(query, [req.params.slug], (err, result) => {
         if (err) throw err
         article = result
         res.render('article',{ article: article });
+    })
+})
+
+app.get('/author/:id', (req, res) => {
+    let query = `SELECT author.name AS author, article.image AS image, article.name AS name, article.slug AS slug FROM article JOIN author ON article.author_id = author.id WHERE author_id = "${req.params.id}";`
+    let articles = [];
+    con.query(query, [req.params.id], (err, result) => {
+        if (err) throw err
+        articles = result
+        author = result[0].author
+        res.render('author',{ author: author, articles: articles });
     })
 })
 
