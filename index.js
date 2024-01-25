f533bb72931642cc95ecda7ad1c77241c7c8eaad
gitTanzj <kalle.riit@gmail.com>
2024-01-25 21:00:52 +0200

    Displayed author article list using the author and article oop model

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const articleControllerClass = require('./controllers/article')
-const articleController = new articleControllerClass()
-
 const articleRoutes = require('./routes/article')
-app.use('/', articleRoutes)
+app.use('/article', articleRoutes)
+
+const authorRoutes = require('./routes/author')
+app.use('/author', authorRoutes)
+
+
+
 
 app.listen(3025, () => {
     console.log('Server is running on port 3025')
--- routes/article.js
+++ routes/article.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const articleControllerClass = require('../controllers/article');
 const articleController = new articleControllerClass();
 
+
+
 router.get('/', (req, res) => articleController.getAllArticles(req, res));
 router.get('/article/:slug', (req,res) => articleController.getArticleBySlug(req, res))
 
